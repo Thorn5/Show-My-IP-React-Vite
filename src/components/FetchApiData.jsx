3f61f7c54e92106595e38a4f799cd6409c0c5937
Trygve Horn <trygvehorn@gmail.com>
2023-10-22 23:39:38 +0200

    Cleaned up

--- src/components/FetchApiData.jsx
+++ src/components/FetchApiData.jsx
@@ -2,15 +2,11 @@
 import useAsyncAwait from "../hooks/useAsyncAwait";
 import useMockData from "../hooks/useMockData";
 import useMockWeatherData from "../hooks/useMockWeatherData";
-// import 'dotenv/config';
-// import dotenv from 'dotenv';
-// dotenv.config();
 
 export default function FetchApiData() {
   // const { loading: ipLoading, error: ipError, apiData: ipApiData, moduleCalled: ipModuleCalled } = useMockData();
 
   const ipBaseUrl = import.meta.env.VITE_APP_IPBASEURL; //useAsyncAwait
-  // const ipBaseUrl = process.env.REACT_APP_IPBASEURL; //useAsyncAwait
   const ipRequestEndpoint = "/json/"; 
   const ipUrl = `${ipBaseUrl}${ipRequestEndpoint}`;
   const { loading: ipLoading, error: ipError, apiData: ipApiData, moduleCalled: ipModuleCalled } = useAsyncAwait(ipUrl);
@@ -23,10 +19,8 @@ export default function FetchApiData() {
   // const { loading: weatherLoading, error: weatherError, apiData: weatherApiData, moduleCalled: weatherModuleCalled } = useMockWeatherData();
 
   const weatherBaseUrl = import.meta.env.VITE_APP_WEATHER_BASE_URL
-  // const weatherBaseUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
   const WeatherRequestEndpoint = `/${ipApiData.city}/today`;
   const weatherRequestParams = import.meta.env.VITE_APP_WEATHER_REQUEST_PARAMS;
-  // const weatherRequestParams = '?unitGroup=metric&include=current&key=BH57DG4255XXKUS2GWMRQG2T6&contentType=json';
   const weatherUrl = `${weatherBaseUrl}${WeatherRequestEndpoint}${weatherRequestParams}`;
   const { loading: weatherLoading, error: weatherError, apiData: weatherApiData, moduleCalled: weatherModuleCalled } = useAsyncAwait(weatherUrl);
 

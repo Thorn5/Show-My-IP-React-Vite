0f3758c156172f82bbda677a1ff8ff8eed15870b
Trygve Horn <trygvehorn@gmail.com>
2023-10-23 13:33:30 +0200

    fixed environmental vars?

--- src/components/FetchApiData.jsx
+++ src/components/FetchApiData.jsx
@@ -7,27 +7,24 @@ export default function FetchApiData() {
   // const { loading: ipLoading, error: ipError, apiData: ipApiData, moduleCalled: ipModuleCalled } = useMockData();
 
   // const ipBaseUrl = import.meta.env.VITE_APP_IPBASEURL; // get .env secret (local repo)
-  const ipBaseUrl = ${{ secrets.VITE_APP_IPBASEURL }}; // get .env secret (GitHub Pages)
-  const ipRequestEndpoint = "/json/"; // true location
-  // const ipRequestEndpoint = "/183.177.124.136/json/"; // mock location
-  const ipUrl = `${ipBaseUrl}${ipRequestEndpoint}`;
-  const { loading: ipLoading, error: ipError, apiData: ipApiData, moduleCalled: ipModuleCalled } = useAsyncAwait(ipUrl);
+const ipRequestEndpoint = "/json/"; // true location
+// const ipRequestEndpoint = "/183.177.124.136/json/"; // mock location
+const ipUrl = `${ipBaseUrl}${ipRequestEndpoint}`;
+const { loading: ipLoading, error: ipError, apiData: ipApiData, moduleCalled: ipModuleCalled } = useAsyncAwait(ipUrl);
 
-  sessionStorage.clear();
-  sessionStorage.setItem("ipData", JSON.stringify(ipApiData));
-  sessionStorage.setItem("ipModuleCalled", JSON.stringify(ipModuleCalled));
+sessionStorage.clear();
+sessionStorage.setItem("ipData", JSON.stringify(ipApiData));
+sessionStorage.setItem("ipModuleCalled", JSON.stringify(ipModuleCalled));
 
-  // const { loading: weatherLoading, error: weatherError, apiData: weatherApiData, moduleCalled: weatherModuleCalled } = useMockWeatherData();
+// const { loading: weatherLoading, error: weatherError, apiData: weatherApiData, moduleCalled: weatherModuleCalled } = useMockWeatherData();
 
-  // const weatherBaseUrl = import.meta.env.VITE_APP_WEATHER_BASE_URL // get .env secret (local repo)
-  const weatherBaseUrl = ${{ secrets.VITE_APP_WEATHER_BASE_URL }}; // get .env secret (GitHub Pages)
-  const WeatherRequestEndpoint = `/${ipApiData.city}/today`;
-  // const weatherRequestParams = import.meta.env.VITE_APP_WEATHER_REQUEST_PARAMS; // get .env secret (local repo)
-  const weatherRequestParams = ${{ secrets.VITE_APP_WEATHER_REQUEST_PARAMS }}; // get .env secret (GitHub Pages)
-  const weatherUrl = `${weatherBaseUrl}${WeatherRequestEndpoint}${weatherRequestParams}`;
-  const { loading: weatherLoading, error: weatherError, apiData: weatherApiData, moduleCalled: weatherModuleCalled } = useAsyncAwait(weatherUrl);
+// const weatherBaseUrl = import.meta.env.VITE_APP_WEATHER_BASE_URL // get .env secret (local repo)
+const WeatherRequestEndpoint = `/${ipApiData.city}/today`;
+// const weatherRequestParams = import.meta.env.VITE_APP_WEATHER_REQUEST_PARAMS; // get .env secret (local repo)
+const weatherUrl = `${weatherBaseUrl}${WeatherRequestEndpoint}${weatherRequestParams}`;
+const { loading: weatherLoading, error: weatherError, apiData: weatherApiData, moduleCalled: weatherModuleCalled } = useAsyncAwait(weatherUrl);
 
-  sessionStorage.setItem("weatherData", JSON.stringify(weatherApiData));
-  sessionStorage.setItem("weatherModuleCalled", JSON.stringify(weatherModuleCalled));
+sessionStorage.setItem("weatherData", JSON.stringify(weatherApiData));
+sessionStorage.setItem("weatherModuleCalled", JSON.stringify(weatherModuleCalled));
 
 }
